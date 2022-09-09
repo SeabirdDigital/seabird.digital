39a6b49a915459cf22bfc66c50722b8884274ba3
Filip Mårtensson <fenwikk@protonmail.com>
2022-09-09 18:09:56 +0200

    Create project and added site

---
+++ src/lib/components/Box2.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    let y: number = 0
+</script>
+
+<svelte:window bind:scrollY={y} />
+
+<div class="absolute -left-[150px] -bottom-[150px] -rotate-[17deg] w-[450px] 2xl:w-[550px] h-[350px] 2xl:h-[450px] bg-sky-400"/>
+<div class="absolute -left-[150px] w-[450px] 2xl:w-[550px] h-[350px] 2xl:h-[450px] border-4 border-black" style="bottom: {y * -.01 - 150}px;transform: rotate({-26 + y * -.01}deg);"/>
--- src/lib/supabase/stores/profile.ts
+++
@@ -1,4 +0,0 @@
-import { writable } from 'svelte/store';
-import type { Profile } from '../client';
-
-export const profile = writable<Profile | undefined>();
