7dbd5ccaa2d42cb06e397ccecd5748e8d56ccce4
Filip Mårtensson <fenwikk@protonmail.com>
2022-09-11 20:25:01 +0200

    Added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-
 import image from "@astrojs/image";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), image()],
+  site: "https://www.seabird.digital",
+  integrations: [svelte(), tailwind(), image(), sitemap()],
   output: "server",
   adapter: vercel()
 });
