1baa8e4604f94c35648fb0025a40ce673d782579
Filip Mårtensson <fenwikk@protonmail.com>
2022-09-14 06:18:15 +0200

    Add buttons in hero too

---
+++ src/lib/components/reuseable/Button.svelte
@@ -0,0 +1,11 @@
+
+<script lang="ts">
+    export let flipRotation: boolean = false;
+
+    let className: string = ""
+    export { className as class }
+</script>
+
+<button class="px-6 py-3 ml-2 border-4 font-paytone hover:rotate-0 border-black hard-shadow hover:shadow-none {flipRotation ? "rotate-2" : "-rotate-2"} {className}">
+    <slot/>
+</button>
