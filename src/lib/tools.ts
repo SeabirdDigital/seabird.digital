8ee51cb7d2903f122e2ccd3469c5bd89c71a1a15
Filip Mårtensson <filip.martensson@gtg.se>
2022-10-19 11:31:19 +0200

    Dont do animation if its the sam page

--- src/lib/tools.ts
+++ src/lib/tools.ts
@@ -1,6 +1,9 @@
 import { cols, rows, ToggleOverlay } from "./components/Load";
 
 export const goto = (url: string) => {
+    if (url == location.href || url == location.pathname)
+        return;
+
     ToggleOverlay();
     setTimeout(() => {
         location.href = url;
